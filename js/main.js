cd12fae191e7ebbd7c71f501ae9af42c942a3a2e
jinyujeong <yuya92@hanmail.net>
2022-04-19 19:28:43 +0900

    js: javascript

---
+++ js/main.js
@@ -0,0 +1,61 @@
+const navbar = document.querySelector('#header');
+const navbarHeight = navbar.getBoundingClientRect().height;
+const logo = document.querySelector('.navbar__logo__img');
+const logoWhite = 'images/logo.svg';
+const logoDark = 'images/logo-dark.svg';
+const arrowUp = document.querySelector('.top__btn');
+document.addEventListener('scroll',() => {
+    if(window.scrollY > navbarHeight){
+        navbar.classList.add('on');
+        logo.src = logoDark;
+        arrowUp.classList.add('on');
+    }else{
+        navbar.classList.remove('on');
+        logo.src = logoWhite;
+        arrowUp.classList.remove('on');
+    }
+});
+
+arrowUp.addEventListener('click', () => {
+    window.scrollTo({top:0})
+});
+
+window.addEventListener('scroll', () => {
+    let wScroll = window.scrollY;
+    const aboutSection = document.querySelector('#about');
+    if(wScroll >= aboutSection.offsetTop-window.innerHeight / 6){
+        aboutSection.classList.add('show');
+    }else{
+        aboutSection.classList.remove('show');
+    }
+});
+
+// let workBtn = document.querySelectorAll('.work__navi__item');
+// console.log(workBtn);
+// workBtn.addEventListener('click',(e) => {
+//     let type = e.target;
+//     console.log(type);
+// })
+function categorize(e){
+    const type = e.textContent;
+    const projects = document.querySelectorAll('.work__list__item');
+    const projectContainer = document.querySelector('.work__list');
+    const workNavi = document.querySelectorAll('.work__navi__item');
+    projectContainer.classList.add('ani-out');
+    for(p=0; p<workNavi.length; p++){
+        workNavi[p].classList.remove('on');
+    }
+    e.parentNode.classList.add('on');    
+    setTimeout(() => {
+        for(i=0; i<projects.length; i++){
+            let datasetType = projects[i].dataset.type;
+            projects[i].classList.remove('on');
+            if(datasetType === type){
+                projects[i].classList.add('on');
+            }else if(type === 'All'){
+                projects[i].classList.add('on');
+            }
+        };
+        projectContainer.classList.remove('ani-out')
+    }, 300);
+}
